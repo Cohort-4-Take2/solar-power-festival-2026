67cb066e6a505102d64c3e7458670fbea9212fcb
Victor Bagu <victorbagu@gmail.com>
2026-04-02 11:08:06 +1300

    Add festival configuration data

---
+++ script.js
@@ -0,0 +1,12 @@
+// --- GLOBAL FESTIVAL DATA ---
+const festivalConfig = {
+    name: "Solar Power NZ",
+    venue: "Western Springs, Auckland",
+    capacity: 50000,
+    ticketsSold: 48500,
+    isSunny: true,
+    basePrice: 120,
+    lineup: ["Lorde", "Marlon Williams", "Benee"]
+};
+
+// STUDENTS: ADD YOUR FUNCTIONS BELOW THIS LINE
